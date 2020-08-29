f27e26ab353ed3154e607c32e62a9d18b1bdf823
Datmon <danilanpilov@mail.ru>
2020-08-29 19:18:42 +0600

    Add files via upload

--- audio_stelaj/audio_stelaj.ino
+++ audio_stelaj/audio_stelaj.ino
@@ -1,33 +1,32 @@
-#include "arduinoFFT.h"
-#include <driver/adc.h>
-#include "FastLED.h"
-#include <WiFi.h>
-#include <PubSubClient.h>
+#define USE_WIFI 0 //использовать ли MQTT и WIFI
 
+#if (USE_WIFI == 1)
 //настройки WIFI и MQTT
-const char* ssid = "***";
-const char* password = "***";
+const char* ssid = "FSOCIETY";
+const char* password = "WannaHackMe";
 const char* mqtt_server = "192.168.0.34";
 #define mqtt_port 1883
 #define MQTT_USER "datmon"
-#define MQTT_PASSWORD "***"
+#define MQTT_PASSWORD "amabof33"
 #define MQTT_SERIAL_PUBLISH_CH "/ESP32/audioStelaj/tx"
 #define MQTT_SERIAL_RECEIVER_brightness "/ESP32/audioStelaj/brightness"
 #define MQTT_SERIAL_RECEIVER_thisMode "/ESP32/audioStelaj/thisMode"
 #define MQTT_SERIAL_RECEIVER_rainbowSpeed "/ESP32/audioStelaj/rainbowSpeed"
 #define MQTT_SERIAL_RECEIVER_rainbowStep "/ESP32/audioStelaj/rainbowStep"
 
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 WiFiClient wifiClient;
-
 PubSubClient client(wifiClient);
+#endif
 
 //технические настройки матрицы из ленты
 #define xres 8  //ширина  (количество столбов) 
 #define  yres 6  //высота 
 #define trash 600 // шумы
-#define NUM_LEDS 48  //количество светодиодов 
-#define PIN 18  //пин ленты 
+#define numberLeds 48  //количество светодиодов 
+#define ledPin 18  //пин ленты 
 
 //настройки звука
 #define noiseFilter 6 //значение шумов (громкость от 0 до 70)
@@ -35,23 +34,27 @@ PubSubClient client(wifiClient);
 #define powerKoef 4 // коэфф для повышения чувствительности с ростом частот
 
 //настройки радуги
+#define timeFall 100 //время падения столбца
 int brightness = 255;  //типа byte (0...255)
 int rainbowSpeed = 15;  //скоротсь изменения радуги
-#define timeFall 100 //время падения столбца
 int rainbowStep = 10; //шаг радуги между светодиодами
 
 //настройки точки
 //#define timeToFall  1000 //время перед падением точки в миллис
 //#define fallSpeed 500  //скорость падения
 
-CRGB leds[NUM_LEDS];  //у меня стока светодиодов
+#include "arduinoFFT.h"
+#include <driver/adc.h>
+#include "FastLED.h"
 
-arduinoFFT FFT = arduinoFFT(); //у меня ардуино эквалайзер 
+CRGB leds[numberLeds];  //у меня стока светодиодов
+
+arduinoFFT FFT = arduinoFFT(); //у меня ардуино эквалайзер
 
 int thisMode = 1;  //переменная выбора режима
 byte counter = 1;  //счетчик для режима радуги
 int rainbowTimer = 0; //и таймер
-float smoothValue[xres]; //сглаживание разных громкостей 
+float smoothValue[xres]; //сглаживание разных громкостей
 float averK = 0.006;  //коэф для изменения чувствительности
 
 //бегущая точка
@@ -71,14 +74,17 @@ int peaks[xres];
 int yvalue;
 int displayTime[xres];
 int displayvalue[xres];
+byte color;
 
 void setup() {
   Serial.begin(115200);
-  Serial.setTimeout(500); // Set time out for wifi
+#if (USE_WIFI == 1)
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
   reconnect();
+  Serial.setTimeout(500); // Set time out for wifi
+#endif
 
   //настройка порта
   adc1_config_width(ADC_WIDTH_BIT_12);
@@ -86,22 +92,24 @@ void setup() {
   pinMode(5, INPUT);
 
   //лента
-  FastLED.addLeds<WS2812B, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.addLeds<WS2812B, ledPin, GRB>(leds, numberLeds).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(brightness);
   pinMode(18, OUTPUT);
 }
 
 void loop() {
+#if (USE_WIFI == 1)
   client.loop();  //функция для MQTT
 
-   //читаю терминал 
-   if (Serial.available() > 0) {
-     char mun[501];
-     memset(mun,0, 501);
-     Serial.readBytesUntil( '\n',mun,500);
-     publishSerialData(mun);
-   }
-  //выбираю режим 
+  //читаю терминал
+  if (Serial.available() > 0) {
+    char mun[501];
+    memset(mun, 0, 501);
+    Serial.readBytesUntil( '\n', mun, 500);
+    publishSerialData(mun);
+  }
+#endif
+  //выбираю режим
   if (thisMode == 1) {
     /*SAMPLING*/
     for (int i = 0; i < samples; i++)
@@ -134,32 +142,32 @@ void loop() {
     }
     //симуляция звука
     /*
-     if (digitalRead(5)) {
+      if (digitalRead(5)) {
       for (int i; i < xres; i++) {
         peaks[i] = yres;
       }
-    }
-    /*
+      }
+      /*
       Serial.print("Button pressed: ");
       Serial.print(displayvalue[xres - 1]);
       Serial.println(" has been sent");
     */
     /*
-    Serial.print((float)data_avgs[1]);
-    Serial.print( ", ");
-    Serial.print((float)data_avgs[3]);
-    Serial.print( ", ");
-    Serial.println((float)data_avgs[7]);
+      Serial.print((float)data_avgs[1]);
+      Serial.print( ", ");
+      Serial.print((float)data_avgs[3]);
+      Serial.print( ", ");
+      Serial.println((float)data_avgs[7]);
     */
     for (int i = 0; i < xres; i++)
     {
-      if (data_avgs[i] < noiseFilter) data_avgs[i] = 0;
-      smoothValue[i] = (float) data_avgs[i] * averK + smoothValue[i] * (1 - averK);
+      if (data_avgs[i] < noiseFilter) data_avgs[i] = 0; //отсекаю низжний порог шумов
+      smoothValue[i] = (float) data_avgs[i] * averK + smoothValue[i] * (1 - averK); //высчитываю переменную для адаптации под громкость
       //Serial.println(smoothValue[0]);
-      data_avgs[i] = constrain(data_avgs[i], noiseFilter, micSensetive + smoothValue[i]);
-      data_avgs[i] = map(data_avgs[i], noiseFilter, micSensetive - (i * powerKoef) + smoothValue[i], 0, yres);
+      data_avgs[i] = constrain(data_avgs[i], noiseFilter, micSensetive - (i * powerKoef) + smoothValue[i]); //ограничиваю массив
+      data_avgs[i] = map(data_avgs[i], noiseFilter, micSensetive - (i * powerKoef) + smoothValue[i], 0, yres);  //маштабирую массив под размеры матрицы
       yvalue = data_avgs[i];
-      if (millis() > displayTime[i] && peaks[i] != 0) {
+      if (millis() > displayTime[i] && peaks[i] != 0) { //плавное падение
         displayTime[i] = millis() + timeFall;
         peaks[i] = peaks[i] - 1;    // decay by one light
         Serial.println(peaks[i]);
@@ -172,9 +180,9 @@ void loop() {
       displayvalue[i] = yvalue;
     }
     ///*
-    Serial.print(displayvalue[1]);
+    Serial.print(displayvalue[5]);
     Serial.print( ", ");
-    Serial.print(displayvalue[3]);
+    Serial.print(displayvalue[6]);
     Serial.print( ", ");
     Serial.println(displayvalue[7]);
     //*/
@@ -184,7 +192,7 @@ void loop() {
 void animation() {
   switch (thisMode) {
     case 0:
-      for (int i = 0; i < NUM_LEDS; i++) {
+      for (int i = 0; i < numberLeds; i++) {
         leds[i] = CHSV(255, 255, 255);
       }
       break;
@@ -192,18 +200,19 @@ void animation() {
       for (int k = 0; k < xres; k++) { //для каждого столбца
         //основные столбцы
         for (int i = 0; i < yres; i++ ) {  //для каждой строчки
-          if (displayvalue[k] > i && k % 2 == 0) leds[(k * yres) + i] = CHSV(counter + (i * rainbowStep), 255, 255); //проверяю длину столбца и заливаю радугой
-          else if (displayvalue[k] > i && k % 2 == 1) leds[(k * yres) + yres - i - 1] = CHSV(counter + (i * rainbowStep), 255, 255); //проверяю длину и четность столбца и заливаю радугой
+          color = counter + (i * rainbowStep) + (k * xres);
+          if (displayvalue[k] > i && k % 2 == 0) leds[(k * yres) + i] = CHSV(color, 255, 255); //проверяю длину столбца и заливаю радугой
+          else if (displayvalue[k] > i && k % 2 == 1) leds[(k * yres) + yres - i - 1] = CHSV(color, 255, 255); //проверяю длину и четность столбца и заливаю радугой
           else if (k % 2 == 0) leds[(k * yres) + i] = CHSV(0, 255, 0);  //остальные крашу в черный
           else if (k % 2 == 1) leds[(k * yres) + yres - i - 1] = CHSV(0, 255, 0);  //остальные крашу в черный
           //Serial.println(displayvalue[1]);
         }
         /*
-        Serial.print(rainbowSpeed);
-        Serial.print(", ");
-        Serial.println(rainbowStep);
+          Serial.print(rainbowSpeed);
+          Serial.print(", ");
+          Serial.println(rainbowStep);
         */
-        
+
         /*//бегущая точка
           if (displayvalue[k] > maxPoint[k]) {  //если есть значение в столбе
           maxPoint[k] = displayvalue[k] + (k * yres);  //добавляю макспоинт
@@ -237,12 +246,13 @@ void animation() {
       break;
   }
 }
+#if (USE_WIFI == 1)
 void setup_wifi() {
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) { //попытка подключения к WIFI
-      delay(500);
-    }
-    randomSeed(micros());
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { //попытка подключения к WIFI
+    delay(500);
+  }
+  randomSeed(micros());
 }
 void reconnect() {
   // Loop until we're reconnected
@@ -252,7 +262,7 @@ void reconnect() {
     String clientId = "soundStelaj";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
-    if (client.connect(clientId.c_str(),MQTT_USER,MQTT_PASSWORD)) {
+    if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
       Serial.println("connected");
       //Once connected, publish an announcement...
       client.publish("/icircuit/presence/ESP32/", "hello world");
@@ -268,26 +278,27 @@ void reconnect() {
   }
 }
 void callback(char* topic, byte *payload, unsigned int length) {
-   if (String(topic) == "/ESP32/audioStelaj/brightness") { //меняется ручками значение канала, дефайн не меняет скобочки
+  if (String(topic) == "/ESP32/audioStelaj/brightness") { //меняется ручками значение канала, дефайн не меняет скобочки
     brightness = map(payload[0], 48, 57, 0, 255); //настройка яркости подсветки
     FastLED.setBrightness(brightness);
-   }
-   if (String(topic) == "/ESP32/audioStelaj/thisMode") thisMode = payload[0] - 48; //выбор режима подсветки
-   if (String(topic) == "/ESP32/audioStelaj/rainbowSpeed") rainbowSpeed = map(payload[0] - 48, 0, 9, 0, 20);
-   if (String(topic) == "/ESP32/audioStelaj/rainbowStep") rainbowStep = map(payload[0] - 48, 0, 9, 0, 20);
-   /*
-   Serial.println("-------new message from broker-----");
-   Serial.print("channel:");
-   Serial.println(topic);
-   Serial.print("data:");  
-   Serial.write(payload, length);
-   Serial.println();
-   if ((char)payload[0] == '0') Serial.println("it's wotking!!!");
-    */
+  }
+  if (String(topic) == "/ESP32/audioStelaj/thisMode") thisMode = payload[0] - 48; //выбор режима подсветки
+  if (String(topic) == "/ESP32/audioStelaj/rainbowSpeed") rainbowSpeed = map(payload[0] - 48, 0, 9, 0, 20);
+  if (String(topic) == "/ESP32/audioStelaj/rainbowStep") rainbowStep = map(payload[0] - 48, 0, 9, 0, 20);
+  /*
+    Serial.println("-------new message from broker-----");
+    Serial.print("channel:");
+    Serial.println(topic);
+    Serial.print("data:");
+    Serial.write(payload, length);
+    Serial.println();
+    if ((char)payload[0] == '0') Serial.println("it's wotking!!!");
+  */
 }
-void publishSerialData(char *serialData){
+void publishSerialData(char *serialData) {
   if (!client.connected()) {
     reconnect();
   }
   client.publish(MQTT_SERIAL_PUBLISH_CH, serialData);
 }
+#endif
